482b4e6f14128729fdd5423de364a6afc639050e
Prakhar <srivprakhar@gmail.com>
2016-01-07 01:05:22 +0530

    first commit: added basic bootstrap styling

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+	<div class="page-header">
+	  <h1>Stocksplosion  <small class='help-text'>Trading apis</small></h1>
+	</div>
+	<div class="row">
+		<form action="" method="POST" class="form-horizontal" role="form">
+				<div class="form-group">
+					<legend>Form title</legend>
+				</div>
+				<div class="form-group">
+					<label for="input" class="col-sm-2 control-label">Company symbol : </label>
+					<div class="col-sm-10">
+						<input type="text" name="" id="input" class="form-control" value="" placeholder="Please provide the company stock symbol" required="required" pattern="" title="">
+					</div>
+				</div>
+				
+		
+				<div class="form-group">
+					<div class="col-sm-10 col-sm-offset-2">
+						<button type="submit" class="btn btn-primary">Submit</button>
+					</div>
+				</div>
+		</form>	
+	</div>
+</div>
+<script  type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
