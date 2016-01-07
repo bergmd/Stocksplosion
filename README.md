b52e568d6750b7b99255ee9bf14ab56ed4d6aaec
Prakhar <srivprakhar@gmail.com>
2016-01-07 21:32:09 +0530

    added comments and installation instructions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Stocksplosion
 ## System requirements and installation instructions
+### System requirements
+* Apache Web Server [Preferably 2.2 or above]
+* PHP extension enabled [Mostly enabled by default on apache webservers]
+* PHP-cURL extension enabled on apache webs server
+
+### Installation Instructions
+* Download all the files to the htdocs directory under root of Apache Web Server installation
+* Start the Apache Web Server
+* Go to url http://{serverurl}/Stocksplosion [ For example on your localhost the url would be http://localhost/Stocksplosion] 
--- index.php
+++ index.php
@@ -1,58 +1,95 @@
 <?php
-$error_name = "";
-$error_code = "";
-$error_exists = false;
-$company_name = "";
-$company_symbol = "";
-if(isset($_POST['stock_name']) && isset($_POST['search'])){
-$stock_name = trim($_POST['stock_name']);
-$recommendation_text = "Random Recommendation Text";
-$recommendation = "";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://stocksplosion.apsis.io/api/company/$stock_name?startdate=20150501&enddate=20150501");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
-$response = curl_exec($ch);
-if ($response === false)
-{
-    $error_name = curl_errno($ch);
-    $error_code = curl_error($ch);
-    $error_exists = true;
-}
-else
-{
-    $response =  json_decode($response);
-    if(empty($response) || is_null($response)){
-    $error_name = "Incorrect Company code";
-$error_code = 1000;
-    $error_exists = true;
+    // Variables
+    $error_name = "";
+    $error_code = "";
+    $error_exists = false;
+    $company_name = "";
+    $company_symbol = "";
+    
+    // This is a self posted form, So check if the for has a POST request or accessed directly
+    if(isset($_POST['stock_name']) && isset($_POST['search']))
+    {
+        // Get the POST data if the form has been filled
+        $stock_name = trim($_POST['stock_name']);
+        // Recommendation text and the recommendation action
+        $recommendation_text = "Random Recommendation Text";
+        $recommendation = "";
+
+        // initialize curl
+        $ch = curl_init();
+        // set curl options and setting
+        curl_setopt($ch, CURLOPT_URL, "http://stocksplosion.apsis.io/api/company/$stock_name?startdate=20150501&enddate=20150501");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        
+        // process the request and get the results back
+        $response = curl_exec($ch);
+
+        // if the request fails
+        if ($response === false)
+        {
+            // get the error code and error message
+            $error_name = curl_errno($ch);
+            $error_code = curl_error($ch);
+            $error_exists = true;
+        }
+        // successful response
+        else
+        {
+            // convert the response to PHP object
+            $response =  json_decode($response);
+
+            
+            /*
+            * The section below handles the response from the API. App sets up recommendations here
+            * This section currently contains arbitrary rules. 
+            * For real application these rules should be rewritten as per business rules
+            */
+            if(empty($response) || is_null($response)){
+                // populate custom error message if there is no response
+                // please provide more details in case of rel application here
+                $error_name = "Incorrect Company code";
+                $error_code = 1000;
+                $error_exists = true;
+            }
+            else
+            {
+                // If the response from the api contains proper data
+                // get the company name
+                $company_name = $response->company->name;
+                // get the compane ticker code
+                $company_symbol = $response->company->symbol;
+
+                // BUSINESS RULES TO BUY , SELL or WAIT 
+                // Buy stock : if Company code contains a vowel
+                if(preg_match_all('/[aeiou]/i', $stock_name, $ignore) > 0){
+                    $recommendation_text = "Buy Stocks : This stock contains a vowel.";
+                    $recommendation = "Buy";
+                }
+                // WAIT : If the company code does not have a vowel and has either of [b, c, d, f, g]
+                elseif (preg_match_all('/[bcdfg]/i', $stock_name, $ignore) > 0){
+                    $recommendation_text = "Wait : This stock contains either of [b, c, d, f, g]";
+                    $recommendation = "Wait";
+                }
+                // SELL stocks : if company does not have a vowel or either of [b, c, d, f, g]
+                else{
+                    $recommendation_text = "Sell Stock : Does not have a vowel and Does not contain [b, c, d, f, g]";
+                    $recommendation = "Sell";
+                }
+            }
+        }
+        // Close the curl connection after each request
+        curl_close($ch);
     }
-    else{
-    $company_name = $response->company->name;
-$company_symbol = $response->company->symbol;
-if(preg_match_all('/[aeiou]/i', $stock_name, $ignore) > 0){
-$recommendation_text = "Buy Stocks : This stock contains a vowel.";
-$recommendation = "Buy";
-}
-elseif (preg_match_all('/[bcdfg]/i', $stock_name, $ignore) > 0){
-$recommendation_text = "Wait : This stock contains either of [b, c, d, f, g]";
-$recommendation = "Wait";
-}
-else{
-$recommendation_text = "Sell Stock : Does not have a vowel and Does not contain [b, c, d, f, g]";
-$recommendation = "Sell";
-}
-    }
-}
-curl_close($ch);
-}
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Test</title>
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <!-- custom styles -->
         <style>
+
             .bg-inverse {background:#444;color:#fff;}
             .bg-grey    {background:#eee;color:#222;}
             .padding-is {padding:60px 0px 60px 0px;}
@@ -67,23 +104,22 @@ curl_close($ch);
         </div>
         <div class="container-fluid bg-grey padding-is">
             <h3 class="text-center">Stocksplosion <small>[API for stock recommendations]</small></h3>
-            <!-- <div class='text-center'><small>Please enter your stock symbol for testing</small></div> -->
             <hr/>
             <form action="" method="POST" class="form-horizontal" role="form">
                 <div class="col-lg-4 col-lg-offset-4 col-sm-8 col-sm-offset-2 col-xs-12">
                     <div class="input-group">
                         <input type="text" class="form-control" name="stock_name" placeholder="Please provide the company stock symbol" required="required" >
                         <span class="input-group-btn">
-                        <!-- <button class="btn btn-default" type="button">Go!</button> -->
                         <input type="submit" name="search" class="btn btn-primary text-capitalize" value="Go!!">
                         </span>
-                        </div><!-- /input-group -->
+                        </div>
                     </div>
                 </form>
                 <br/><br/><br/>
             </div>
             <div class="container">
                 <br/>
+                <!-- if there are any error only then show the below section -->
                 <?php if($error_exists): ?>
                 <div class="alert alert-danger clearfix">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
@@ -93,9 +129,9 @@ curl_close($ch);
                         <li>Error Code : <?=$error_code;?></li>
                     </ul>
                 </div>
+                <!-- if there is a proper response fom the api then show the recommendaions -->
                 <?php elseif(!$error_exists && isset($_POST['search'])): ?>
                 <div class="alert alert-success clearfix">
-                    <!-- <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> -->
                     <strong>The search produced below results</strong>
                     <div class="row">
                         <div class="col-xs-12">Company Name : <?=$company_name?></div>
@@ -110,6 +146,7 @@ curl_close($ch);
                     </div>
                 </div>
                 <?php endif; ?>
+                <!-- The below section shows some sample data which could be used for testing -->
                 <div class="panel-group" id="accordion" role="tablist" >
                     <div class="panel panel-default">
                         <div class="panel-heading" role="tab" id="headingOne">
